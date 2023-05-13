f9978a60aef7c9cb4a464a49cf0381abeb43605f
Sanya <sashulya1006@mail.ru>
2023-05-13 14:48:21 +0300

    Добавьте файлы проекта.

---
+++ Linq/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+public class Program
+{
+    public static void Main()
+    {
+        int[] mass = { -2, -5, 0, 5, 7, 9, 10, 12, 31 };
+        var negativeNums = mass.Count(x => x < 0);
+        var positiveNums = mass.Count(x => x > 0);
+        var evenSum = mass.Where(x => x % 2 == 0).Sum();
+
+        Console.WriteLine($"Кол-во отрицательных: {negativeNums}");
+        Console.WriteLine($"Кол-во положительных: {positiveNums}");
+        Console.WriteLine($"Сумма чётных: {evenSum}");
+    }
+}
